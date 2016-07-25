17412e953212183cd018442b80ab852087cbc38b
vinodkc <vinod.kc.in@gmail.com>
2016-07-25 22:08:16 +0530

    updated

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,31 @@
-function start() {
- console.log("start called");
+var exec = require("child_process").exec
+function start(response) {
+ 
+ exec("find /", { timeout: 10000, maxBuffer: 20000*1024 },function(error,stdout,stderr){
+  console.log("start called");
+  var body = '<html>'+
+  '<head>'+
+  '<meta http-equiv="Content-Type" content="text/html; '+
+  'charset=UTF-8" />'+
+  '</head>'+
+  '<body>'+
+  '<form action="/upload" method="post">'+
+  '<textarea name="text" rows="20" cols="60"></textarea>'+
+  '<input type="submit" value="Submit text" />'+
+  '</form>'+
+  '</body>'+
+ '</html>';
+ response.writeHead(200, {"Content-Type": "text/html"});
+ response.write(body);
+ response.end();
+
+});
 }
-function upload() {
+function upload(response) {
  console.log("upload called");
+ response.writeHead(200, {"Content-Type": "text/plain"});
+ response.write("Hello upload");
+ response.end();
 }
 exports.start = start;
 exports.upload = upload;
--- server.js
+++ server.js
@@ -4,11 +4,7 @@ function start(route,handle) {
  function onRequest(request, response) {
 	console.log("Request received");
 	var pathname = url.parse(request.url).pathname;
-	console.log("Request for " + pathname  + " received");
-        route(handle,pathname);
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	response.end();
+        route(handle,pathname,response);
 	}
   http.createServer(onRequest).listen(8888);
   console.log("Server Started");
